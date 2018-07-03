5a8abf76040bdffc660c3aa2534544fb3f0bb82d
Ricardo Mtz <ricmtz15@gmail.com>
2018-07-02 23:16:01 -0500

    Implementation of funcion to evaluete the fitness of the population

--- Diff_evolution.py
+++ Diff_evolution.py
@@ -1,5 +1,6 @@
 from scipy.io import loadmat
 import numpy as np
+import math
 
 FILE_PARAM = loadmat('mediciones.mat')
 
@@ -24,35 +25,98 @@ LAM_MIN = 0.1090
 LAM_MAX = 0.1107
 
 
-def w_function(i_time: int) -> float:
+def w_function(i_time, j, lam):
+    """
+    Parameters
+    ----------
+    i_time: int
+
+    j : numpy.array
+
+    lam : numpy.array
+    """
+    ia = I_ALPHA[0][i_time]
+    w = W[0][i_time]
+    ci = CI[0][i_time]
+    return (lam/j)*(-ia * math.sin(P*w)) - (F/j) * w - (ci/j)
+
+
+def i_alpha_function(i_time, r, l, lam):
+    """
+    Parameters
+    ----------
+    i_time : int
+
+    r : numpy.array
+
+    l : numpy.array
+
+    lam : numpy.array
+    """
+    ia = I_ALPHA[0][i_time]
+    ua = U_ALPHA[0][i_time]
+    w = W[0][i_time]
+    return ((r/l)*ia+(P*lam/l)*w*math.sin(w)+(1/l)*ua)
+
+
+def i_beta_function(i_time, r, l, lam):
+    """
+    Parameters
+    ----------
+    i_time : int
+
+    r : numpy.array
+
+    l : numpy.array
+
+    lam : numpy.array
+    """
+    ib = I_BETA[0][i_time]
+    ub = U_BETA[0][i_time]
+    w = W[0][i_time]
+    return ((r/l)*ib+(P*lam/l)*w*math.cos(w)+(1/l)*ub)
+
+
+def calc_error(i_time, r, l, j, lam):
+    """
+    Parameters
+    ----------
+    i_time : int
+
+    r : numpy.array
+
+    l : numpy.array
+
+    j : numpy.array
+
+    lam : numpy.array
+    """
+    print(w_function(i_time, j, lam))
     pass
 
 
-def i_alpha_function(i_time: int) -> float:
-    pass
+def fitness_function(i_time, population_in):
+    """
+    Parameters
+    ----------
+    i_time: int
 
+    population_in : list
 
-def i_beta_function(i_time: int) -> float:
-    pass
-
-
-def error_function(iap: float, ibp: float, wp: float) -> float:
-    pass
-
-
-def fitness_function(i_time: int, population_in: list):
-    pass
+    """
+    for i in population_in:
+        i['error'] = calc_error(i_time, i['R'], i['L'], i['J'], i['LAM'])
 
 
 def create_pop(pop_size: int, problem_size: int) -> list:
     population = []
     for _ in range(pop_size):
-        temp = {}
-        temp['R'] = (np.random.uniform(R_MIN, R_MAX, problem_size))
-        temp['L'] = (np.random.uniform(L_MIN, L_MAX, problem_size))
-        temp['J'] = (np.random.uniform(J_MIN, J_MAX, problem_size))        
-        temp['LAM'] = (np.random.uniform(LAM_MIN, LAM_MAX, problem_size))
-        population.append(temp)
+        temp_s = {}
+        temp_s['R'] = (np.random.uniform(R_MIN, R_MAX, problem_size))
+        temp_s['L'] = (np.random.uniform(L_MIN, L_MAX, problem_size))
+        temp_s['J'] = (np.random.uniform(J_MIN, J_MAX, problem_size))
+        temp_s['LAM'] = (np.random.uniform(LAM_MIN, LAM_MAX, problem_size))
+        population.append(temp_s)
     return population
 
 
@@ -78,13 +142,15 @@ def select_population():
 
 def search(max_gen: int, pop_size: int, problem_size: int,
            wf: float, cr: float) -> list:
+    pop = create_pop(pop_size, problem_size)
+    fitness_function(0, pop)
     pass
 
 
 def main():
     problem_size = 3
     max_gen = len(FILE_PARAM['time'][0])
-    pop_size = 30
+    pop_size = 5
     weight_f = 0.8
     cross_f = 0.9
     best = search(max_gen, pop_size, problem_size, weight_f, cross_f)
