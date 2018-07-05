681314a352f83f7f61e13ffb9678e7918d198058
Ricardo Mtz <ricmtz15@gmail.com>
2018-07-04 23:06:26 -0500

    Implementation of functions create children, select_parent, select_population and de_rand 1_bin

--- Diff_evolution.py
+++ Diff_evolution.py
@@ -1,4 +1,5 @@
 from scipy.io import loadmat
+from scipy.linalg import norm
 import numpy as np
 import math
 
@@ -10,9 +11,9 @@ CI = FILE_PARAM['ic']  # Load torque
 W = FILE_PARAM['vel']  # Speed
 
 P = 2  # Poles number
-U_ALPHA = 5  # Estandor's voltage
-U_BETA = 5  # Estandor's voltage
-F = 0.5  # Friction coefficient
+U_ALPHA = 0.05  # Estandor's voltage
+U_BETA = 0.05  # Estandor's voltage
+F = 0.0001  # Friction coefficient
 
 # Search space of the parameters
 R_MIN = 0.4
@@ -54,9 +55,8 @@ def i_alpha_function(i_time, r, l, lam):
     lam : numpy.array
     """
     ia = I_ALPHA[0][i_time]
-    ua = U_ALPHA[0][i_time]
     w = W[0][i_time]
-    return ((r/l)*ia+(P*lam/l)*w*math.sin(w)+(1/l)*ua)
+    return ((r/l)*ia+(P*lam/l)*w*math.sin(w)+(1/l)*U_ALPHA)
 
 
 def i_beta_function(i_time, r, l, lam):
@@ -72,9 +72,8 @@ def i_beta_function(i_time, r, l, lam):
     lam : numpy.array
     """
     ib = I_BETA[0][i_time]
-    ub = U_BETA[0][i_time]
     w = W[0][i_time]
-    return ((r/l)*ib+(P*lam/l)*w*math.cos(w)+(1/l)*ub)
+    return ((r/l)*ib+(P*lam/l)*w*math.cos(w)+(1/l)*U_BETA)
 
 
 def calc_error(i_time, r, l, j, lam):
@@ -91,21 +90,26 @@ def calc_error(i_time, r, l, j, lam):
 
     lam : numpy.array
     """
-    print(w_function(i_time, j, lam))
-    pass
+    ia = I_ALPHA[0][i_time]
+    ib = I_BETA[0][i_time]
+    w = W[0][i_time]
+    iap = i_alpha_function(i_time, r, l, lam)
+    ibp = i_beta_function(i_time, r, l, lam)
+    wp = w_function(i_time, j, lam)
+    return abs((norm(ia) - norm(iap)) + (norm(ib) - norm(ibp))
+               + (norm(w - wp)))
 
 
-def fitness_function(i_time, population_in):
+def fitness_function(i_time, pop):
     """
     Parameters
     ----------
     i_time: int
 
-    population_in : list
-
+    pop : list
     """
-    for i in population_in:
-        i['error'] = calc_error(i_time, i['R'], i['L'], i['J'], i['LAM'])
+    for i in pop:
+        i['Error'] = calc_error(i_time, i['R'], i['L'], i['J'], i['LAM'])
 
 
 def create_pop(pop_size: int, problem_size: int) -> list:
@@ -120,30 +124,118 @@ def create_pop(pop_size: int, problem_size: int) -> list:
     return population
 
 
-def random_vector():
-    pass
+def de_rand_1_bin(p0, p1, p2, p3, problem_size, wf, cr):
+    """
+    Parameters
+    ----------
+    p0 : dict
+
+    p1 : dict
+
+    p2 : dict
+
+    p3 : dict
+
+    problem_size : int
+
+    wf : float
+
+    cr : float
+    """
+    cut = np.random.randint(problem_size)
+    sp = {'R': np.zeros(problem_size),
+          'L': np.zeros(problem_size),
+          'J': np.zeros(problem_size),
+          'LAM': np.zeros(problem_size)
+          }
+    for i in range(problem_size):
+        if i == cut or np.random.rand() < cr:
+            sp['R'][i] = p3['R'][i] + wf * (p1['R'][i] - p2['R'][i])
+            sp['R'][i] = sp['R'][i] if sp['R'][i] < R_MAX else R_MAX
+            sp['R'][i] = sp['R'][i] if sp['R'][i] > R_MIN else R_MIN
+            sp['L'][i] = p3['L'][i] + wf * (p1['L'][i] - p2['L'][i])
+            sp['L'][i] = sp['L'][i] if sp['L'][i] < L_MAX else L_MAX
+            sp['L'][i] = sp['L'][i] if sp['L'][i] > L_MIN else L_MIN
+            sp['J'][i] = p3['J'][i] + wf * (p1['J'][i] - p2['J'][i])
+            sp['J'][i] = sp['J'][i] if sp['J'][i] < J_MAX else J_MAX
+            sp['J'][i] = sp['J'][i] if sp['J'][i] > J_MIN else J_MIN
+            sp['LAM'][i] = p3['LAM'][i] + wf * (p1['LAM'][i] - p2['LAM'][i])
+            sp['LAM'][i] = sp['LAM'][i] if sp['LAM'][i] < LAM_MAX else LAM_MAX
+            sp['LAM'][i] = sp['LAM'][i] if sp['LAM'][i] > LAM_MIN else LAM_MIN
+        else:
+            sp['R'][i] = p0['R'][i]
+            sp['L'][i] = p0['L'][i]
+            sp['J'][i] = p0['J'][i]
+            sp['LAM'][i] = p0['LAM'][i]
+    return sp
 
 
-def de_rand_1_bin():
-    pass
+def select_parents(pop, current):
+    """
+    Parameters
+    ----------
+    pop : list
+
+    current: int
+    """
+    p1, p2, p3 = np.random.randint(len(pop), size=3)
+    while (p1 == current):
+        p1 = np.random.randint(len(pop))
+    while (p2 == current or p2 == p1):
+        p2 = np.random.randint(len(pop))
+    while (p3 == current or p3 == p1 or p3 == p2):
+        p3 = np.random.randint(len(pop))
+    return [p1, p2, p3]
 
 
-def select_parents():
-    pass
+def create_children(pop, problem_size, wf, cr):
+    """
+    Parameters
+    ----------
+    pop : list
+
+    problem_size : int
+
+    wf : float
+
+    cr : float
+    """
+    children = []
+    for i, p0 in enumerate(pop):
+        p1, p2, p3 = select_parents(pop, i)
+        children.append(de_rand_1_bin(p0, pop[p1], pop[p2], pop[p3],
+                                      problem_size, wf, cr))
+    return children
 
 
-def create_children():
-    pass
+def select_population(parents, children, pop_size):
+    """
+    Parameters
+    ---------
+    parents : list
 
+    children : list
 
-def select_population():
-    pass
+    pop_size : int
+    """
+    n_pop = []
+    for i in range(pop_size):
+        n_pop.append(parents[i] if parents[i]['Error'] <=
+                     children[i]['Error'] else children[i])
+    return n_pop
 
 
 def search(max_gen: int, pop_size: int, problem_size: int,
            wf: float, cr: float) -> list:
     pop = create_pop(pop_size, problem_size)
-    fitness_function(0, pop)
+    # fitness_function(0, pop)
+    # print(select_parents(pop, 2))
+    for i in pop:
+        print(i)
+    print('_____________________')
+    children = create_children(pop, problem_size, wf, cr)
+    for i in children:
+        print(i)
     pass
 
 
@@ -152,8 +244,8 @@ def main():
     max_gen = len(FILE_PARAM['time'][0])
     pop_size = 5
     weight_f = 0.8
-    cross_f = 0.9
-    best = search(max_gen, pop_size, problem_size, weight_f, cross_f)
+    cross_r = 0.9
+    best = search(max_gen, pop_size, problem_size, weight_f, cross_r)
     print(best)
 
 
