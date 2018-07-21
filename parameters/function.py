43b44f5da2f0d29de3ae05ed6a7e99cbdb66f239
Ricardo Mtz <ricmtz15@gmail.com>
2018-07-21 17:28:45 -0500

    Creation of function file, and creation of few of their methos

---
+++ parameters/function.py
@@ -0,0 +1,68 @@
+from parameters import Parameter as parm
+import math
+
+
+class Function(object):
+
+    @staticmethod
+    def func_w(i_time, j, lam):
+        """
+        Parameters
+        ----------
+        i_time: int
+
+        j : float
+
+        lam : float
+        """
+        ia = parm.get_i_alpha(i_time)
+        ib = parm.get_i_beta(i_time)
+        theta = parm.get_theta(i_time)
+        ci = parm.get_ci(i_time)
+        w = lam/j*(-ia * math.sin(parm.P)*theta + ib *
+                   math.cos(parm.P)*theta) - parm.F/j * theta - ci/j
+        return w*.002
+
+    @staticmethod
+    def i_alpha_function(i_time, r, l, lam):
+        """
+        Parameters
+        ----------
+        i_time : int
+
+        r : float
+
+        l : float
+
+        lam : float
+        """
+        ia = parm.get_i_alpha(i_time)
+        w = theta = parm.get_theta(i_time)
+        d_ia = r/l*ia+parm.P*lam/l*w*math.sin(theta)+1/l*parm.U_ALPHA
+        return d_ia
+
+    @staticmethod
+    def i_beta_function(i_time, r, l, lam):
+        """
+        Parameters
+        ----------
+        i_time : int
+
+        r : float
+
+        l : float
+
+        lam : float
+        """
+        ib = parm.get_i_beta(i_time)
+        w = theta = parm.get_theta(i_time)
+        d_ib = r/l*ib+parm.P*lam/l*w*math.cos(theta)+1/l*parm.U_BETA
+        return d_ib
+
+
+def main():
+    print(Function.func_w(0, 0.0015, 0.1090))
+
+
+if __name__ == '__main__':
+    main()
--- parameters/parameters.py
+++ parameters/parameters.py
@@ -1,13 +1,14 @@
 from scipy.io import loadmat
+import numpy as np
 
 
 class Parameter(object):
     FILE_PARAM = loadmat('mediciones.mat')
 
-    I_ALPHA = FILE_PARAM['ia']  # Stator current
-    I_BETA = FILE_PARAM['ib']  # Stator current
-    CI = FILE_PARAM['ic']  # Load torque
-    THETA = FILE_PARAM['vel']  # Speed
+    I_ALPHA = FILE_PARAM['ia'][0]  # Stator current
+    I_BETA = FILE_PARAM['ib'][0]  # Stator current
+    CI = FILE_PARAM['ic'][0]  # Load torque
+    THETA = FILE_PARAM['vel'][0]  # Speed
 
     P = 2  # Poles number
     U_ALPHA = 0.00005  # Estandor's voltage
@@ -23,3 +24,43 @@ class Parameter(object):
     J_MAX = 0.00030
     LAM_MIN = 0.1090
     LAM_MAX = 0.1107
+
+    @staticmethod
+    def get_rand_R():
+        return np.random.uniform(Parameter.R_MIN, Parameter.R_MAX)
+
+    @staticmethod
+    def get_rand_L():
+        return np.random.uniform(Parameter.L_MIN, Parameter.L_MAX)
+
+    @staticmethod
+    def get_rand_J():
+        return np.random.uniform(Parameter.J_MIN, Parameter.J_MAX)
+
+    @staticmethod
+    def get_rand_LAM():
+        return np.random.uniform(Parameter.LAM_MIN, Parameter.LAM_MAX)
+
+    @staticmethod
+    def get_i_alpha(time):
+        return Parameter.I_ALPHA[time]
+
+    @staticmethod
+    def get_i_beta(time):
+        return Parameter.I_BETA[time]
+
+    @staticmethod
+    def get_ci(time):
+        return Parameter.CI[time]
+
+    @staticmethod
+    def get_theta(time):
+        return Parameter.THETA[time]
+
+
+def main():
+    print(Parameter.I_ALPHA)
+
+
+if __name__ == '__main__':
+    main()
