b504c5b33b91761ce3e1950cf954d6ccbb82be7c
ricmtz <ricmtz15@gmail.com>
2018-10-03 13:25:54 -0500

    #8 Modulo util creado, y se agregaron sus funciones a init.py

--- utility/__init__.py
+++ utility/__init__.py
@@ -1,3 +1,4 @@
-from.plotter import Plotter
+from .plotter import Plotter
+from .util import rand, rand_bound, randint_bound, choice
 
-__all__ = ['Plotter']
+__all__ = ['Plotter', 'rand', 'rand_bound', 'randint_bound', 'choice']
