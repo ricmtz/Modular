302cef6f389b94fb3a03494e82860bc2836b921a
ricmtz <ricmtz15@gmail.com>
2018-10-05 11:19:57 -0500

    #14 rand_gaussian function implemented, and rand_gaussian added to all of init.py

--- utility/__init__.py
+++ utility/__init__.py
@@ -1,4 +1,5 @@
 from .plotter import Plotter
-from .util import rand, rand_bound, randint_bound, choice
+from .util import rand, rand_bound, randint_bound, choice, rand_gaussian
 
-__all__ = ['Plotter', 'rand', 'rand_bound', 'randint_bound', 'choice']
+__all__ = ['Plotter', 'rand', 'rand_bound',
+           'randint_bound', 'choice', 'rand_gaussian']
