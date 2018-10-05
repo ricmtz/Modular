1bccda07ce847e0898da5fd67b9f99621990b82b
ricmtz <ricmtz15@gmail.com>
2018-10-05 15:06:06 -0500

    #14 PopulationStrat added to all variable

--- evolutionary/__init__.py
+++ evolutionary/__init__.py
@@ -1,9 +1,11 @@
 from .citizen import Citizen
 from .citizen_s import CitizenStrat
 from .pop_gen import PopulationGen
+from .pop_strat import PopulationStrat
 from .diff_evo import DiffEvolution
 from .evolution_s import EvolutionStrat
 from .genetic import Genetic
 
 __all__ = ['Citizen', 'CitizenStrat', 'DiffEvolution',
-           'EvolutionStrat', 'Genetic', 'PopulationGen']
+           'EvolutionStrat', 'Genetic', 'PopulationGen',
+           'PopulationStrat']
