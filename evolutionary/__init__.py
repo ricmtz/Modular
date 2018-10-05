5a3f5747380001fb6dd7aa2fe94099a71e962824
ricmtz <ricmtz15@gmail.com>
2018-10-05 00:09:42 -0500

    #14 imports order changed

--- evolutionary/__init__.py
+++ evolutionary/__init__.py
@@ -1,9 +1,9 @@
 from .citizen import Citizen
 from .citizen_s import CitizenS
+from .pop_gen import PopulationGen
 from .diff_evo import DiffEvolution
 from .evolution_s import EvolutionS
 from .genetic import Genetic
-from .pop_gen import PopulationGen
 
 __all__ = ['Citizen', 'CitizenS', 'DiffEvolution',
            'EvolutionS', 'Genetic', 'PopulationGen']
