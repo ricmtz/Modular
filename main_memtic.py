00212aca2e7b3bf55020fb7e69f555a97cd95f3c
ricmtz <ricmtz15@gmail.com>
2018-10-06 13:54:54 -0500

    #13 main_memtic changed to use the new Memetic and Plotter class

--- main_memtic.py
+++ main_memtic.py
@@ -1,22 +1,21 @@
-from physical.memetic import Memetic
-from parameters import Parameter as parm
-from plot import Plotter
+from physical import Memetic
+from utility import Plotter
 
 
 def main():
     problem_size = 3
-    search_space = parm.get_bounds()
-    max_gens = 100
-    pop_size = 100
+    max_gens = 2
+    pop_size = 20
     p_cross = 0.98
     p_mut = 1.0/float(problem_size*16)
     max_local_gens = 20
     p_local = 0.5
-    a = Memetic(max_gens, search_space, pop_size,
-                p_cross, p_mut, max_local_gens, p_local)
-    best, error = a.search()
-    print(best.get_values())
-    Plotter.plot_functions(*best.get_values(), error)
+    a = Memetic(max_gens, pop_size, p_cross, p_mut, max_local_gens, p_local)
+    print('Memetic Algorithm')
+    a.search()
+    best = a.get_best()
+    print(*best.get_values())
+    Plotter.plot_result(*best.get_values(), a.get_error(), 'Memetic Algorithm')
 
 
 if __name__ == '__main__':
