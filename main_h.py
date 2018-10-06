8336d3afe35345879049dbee8518501698fae27f
ricmtz <ricmtz15@gmail.com>
2018-10-06 17:16:13 -0500

    #13 main_h changed to make use of Harmony class and the new Plotter class

--- main_h.py
+++ main_h.py
@@ -1,18 +1,18 @@
-from physical.harmony_a import HarmonyA
-from plot import Plotter
+from physical import Harmony
+from utility import Plotter
 
 
 def main():
-    max_gen = 100
+    max_gen = 20
     mem_size = 20
     consid_r = 0.95
     adjust_r = 0.7
     rang = 0.05
-    a = HarmonyA(max_gen, mem_size, consid_r, adjust_r, rang)
-    best, error = a.search()
-    print(best.get_R(), best.get_L(), best.get_J(), best.get_LAM())
-    Plotter.plot_functions(best.get_R(), best.get_L(),
-                           best.get_J(), best.get_LAM(), error)
+    a = Harmony(max_gen, mem_size, consid_r, adjust_r, rang)
+    a.search()
+    best = a.get_best()
+    print(*best.get_values())
+    Plotter.plot_result(*best.get_values(), a.get_error(), 'Harmony Search')
 
 
 if __name__ == '__main__':
