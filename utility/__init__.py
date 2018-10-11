ccd062a63f731b25446acae84b2b32b4e6fccb2c
ricmtz <ricmtz15@gmail.com>
2018-10-10 22:38:29 -0500

    #23 Saver added to all variable

--- utility/__init__.py
+++ utility/__init__.py
@@ -1,5 +1,7 @@
+from .saver import Saver
 from .plotter import Plotter
 from .util import rand, rand_bound, randint_bound, choice, rand_gaussian
 
 __all__ = ['Plotter', 'rand', 'rand_bound',
-           'randint_bound', 'choice', 'rand_gaussian']
+           'randint_bound', 'choice', 'rand_gaussian',
+           'Saver']
