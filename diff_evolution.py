ab340a27da51c9f6e5ece77f1b7ad58836f65e0a
ricmtz <ricmtz15@gmail.com>
2018-10-14 16:39:08 -0500

    #29 modified to implement Runner decorator class

--- diff_evolution.py
+++ diff_evolution.py
@@ -1,7 +1,5 @@
 from evolutionary.diff_evo import DiffEvolution
-from utility import Plotter, Saver
-
-ALGORITHM = 'Differential Evolution'
+from utility import Runner
 
 
 def main():
@@ -9,16 +7,8 @@ def main():
     pop_size = 10
     weight_f = 0.2
     cross_r = 0.7
-    a = DiffEvolution(max_gen, pop_size, weight_f, cross_r)
-    print(ALGORITHM.center(50, '-'))
-    a.search()
-    best = a.get_best()
-    print(''.center(50, '-'))
-    print('Results: ', best.get_values())
-    Plotter.plot_result(*best.get_values(), a.get_error(),
-                        ALGORITHM)
-    Saver.save_results(ALGORITHM,
-                       best.get_values(), a.get_error())
+    algorithm = DiffEvolution(max_gen, pop_size, weight_f, cross_r)
+    Runner.run_algorithm(algorithm, 'Differential Evolution')
 
 
 if __name__ == '__main__':
--- random_search.py
+++ random_search.py
@@ -1,19 +1,11 @@
 from stochastic import RandomSearch
-from utility import Plotter, Saver
-
-ALGORITHM = 'Random Search'
+from utility import Runner
 
 
 def main():
     max_gen = 50
-    a = RandomSearch(max_gen)
-    print(ALGORITHM.center(50, '-'))
-    a.search()
-    best = a.get_best()
-    print(''.center(50, '-'))
-    print('Results: ', best.get_values())
-    Plotter.plot_result(*best.get_values(), a.get_error(), ALGORITHM)
-    Saver.save_results(ALGORITHM, best.get_values(), a.get_error())
+    algorithm = RandomSearch(max_gen)
+    Runner.run_algorithm(algorithm, 'Random Search')
 
 
 if __name__ == '__main__':
