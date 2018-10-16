a4d9f83d525e9883592d446e53ce57e1e681f4da
ricmtz <ricmtz15@gmail.com>
2018-10-16 00:09:58 -0500

    #2 PopulationImmuneNet implemented

--- immune/pop_imm_net.py
+++ immune/pop_imm_net.py
@@ -1,43 +1,78 @@
+from math import exp, sqrt
 from models import Population
-import CitizenImmuneNetwork as CTN
+from immune import Cell
+from utility import rand_gaussian
 
-class PopulationImmuneNetwork(Population):
-    def __init__(self, pop_size):
+
+class PopulationImmuneNet(Population):
+    def __init__(self, pop_size, num_clones, beta, num_rand):
         super().__init__(pop_size)
+        self.num_clones = num_clones
+        self.beta = beta
+        self.num_rand = num_rand
+        self.aff_tresh = [(bound[1]-bound[0])*0.05 for bound in Cell.BOUNDS]
 
     def create_pop(self):
-        return [CTN.CitizenImmuneNetwork() for i in range(self.pop_size)]
+        return [Cell() for _ in range(self.pop_size)]
 
-    def get_worst(self):
-        return self.population[-1]
+    def clone(self, parent):
+        return Cell(parent.get_values())
+
+    def mutation_rate(self, normalized_cost):
+        return (1.0/self.beta) * exp(-normalized_cost)
+
+    def mutate(self, child, normalized_cost):
+        for i, v in enumerate(child.get_values()):
+            alpha = self.mutation_rate(normalized_cost)
+            child.set_value(i, (v + alpha * rand_gaussian()))
+
+    def clone_cell(self, parent):
+        clones = [self.clone(parent) for i in range(self.num_clones)]
+        for cell in clones:
+            self.mutate(cell, parent.get_norm_error())
+        self.evaluate(clones)
+        self.sort_pop(clones)
+        return clones[0]
 
     def calculate_normalized_error(self):
         self.sort_pop()
-        diff = self.get_best().get_error() - self.get_worst().get_error()
-        self.set_normalized_errors(diff)
-
-    def set_normalized_errors(diff):
-        if diff == 0.0:
-            for citizen in self.population:
-                citizen.set_normalized_error(1.0)
+        ran = self.get_worst().get_error() - self.get_best().get_error()
+        if ran == 0.0:
+            for p in self.population:
+                p.set_norm_error(1.0)
         else:
-            for citizen in self.population:
-                citizen.set_normalized_error(1.0 - (citizen.get_error() / diff))
+            for p in self.population:
+                p.set_norm_error(1.0 - (p.get_error() / ran))
 
-    def get_average_error():
-        sum = 0.0
-        for citizen in self.population:
-            sum += citizen.get_error()
-        return sum / float(len(self.population))
+    def average_error(self, pop=None):
+        pop = pop if pop else self.population
+        total = sum(c.get_error() for c in pop)
+        return total / float(len(pop))
 
-    def clone_cells(beta, num_clones):
-        cloned_pop = []
-        for cell_index in range(len(self.population)):
-            cloned_pop.append(self.clone_cell(beta, num_clones, self.population[cell_index]))
+    def distance(self, c1, c2):
+        total = sum((x + y)**2.0 for x, y in zip(c1, c2))
+        return sqrt(total)
 
-    def clone_cell(beta, num_clones, parent):
-        return CTN.CitizenImmuneNetwork(parent.parameters)
+    def get_neighborhood(self, cell, pop):
+        neighbors = []
+        for p in pop:
+            if self.distance(p.get_values(), cell.get_values()) < \
+                    self.aff_thresh:
+                neighbors.append(p)
+        return neighbors
 
-    def mutate_cells(pop, beta, norm_err):
+    def get_progeny(self):
+        return [self.clone_cell(p) for p in self.population]
+
+    def affinity_supress(self, pop):
+        pop = []
         for cell in pop:
-            cell.mutate(beta, norm_err)
+            neighbors = self.get_neighborhood(cell, pop)
+            self.sort_pop(neighbors)
+            if (not len(neighbors)) or (cell == neighbors[0]):
+                pop.append(cell)
+        self.population = pop
+
+    def add_rand_pop(self):
+        for _ in range(self.num_rand):
+            self.population.append(Cell())
