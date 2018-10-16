44cd2b0cf2d6b6743d80762d3547cdc4929f2c31
ricmtz <ricmtz15@gmail.com>
2018-10-16 00:10:25 -0500

    #2 ImmuneNetwork class implemented

--- immune/immune_network.py
+++ immune/immune_network.py
@@ -1,37 +1,35 @@
 from models import Algorithm
-from models import Population
-import PopulationImmuneNetwork as PIN
+from immune import PopulationImmuneNet
+
 
 class ImmuneNetwork(Algorithm):
-    def __init__(self, pop_size, max_gen, beta=100, num_clones=10, ):
+    def __init__(self, max_gen, pop_size, num_clones, beta, num_rand):
         super().__init__()
         self.pop_size = pop_size
         self.max_gen = max_gen
+        self.num_clones = num_clones
         self.beta = beta
+        self.num_rand = num_rand
 
     def search(self):
         gen = 0
-
-        pop = PIN.PopulationImmuneNetwork(self.pop_size)
-        pop.evaluate()
-
+        pop = PopulationImmuneNet(
+            self.pop_size, self.num_clones, self.beta, self.num_rand)
         while gen < self.max_gen:
             pop.evaluate()
-            pop.calculate_normalized_cost()
+            pop.calculate_normalized_error()
             pop.sort_pop()
-
-            if self.best is None or pop.get_best().get_error() < self.best.get_error():
+            if self.best is None or \
+                    pop.get_best().get_error() < self.best.get_error():
                 self.best = pop.get_best()
-
+            self.error.append(self.best.get_error())
             progeny = None
-            avg_err = pop.get_average_error()
-
+            avg_err = pop.average_error()
             while True:
-                progeny = [clone_cell(beta, num_clones, pop[i]) for i in range(len(pop))]
-                if average_cost(progeny) < avgCost:
+                progeny = pop.get_progeny()
+                if pop.average_error(progeny) < avg_err:
                     break
-            pop = affinity_supress(progeny, aff_thresh)
-            [pop.append({"vector": random_vector(search_space)}) for i in range(num_rand)]
-            print(f" > gen #{gen+1}, popSize=#{len(pop)}, fitness=#{best['cost']}")
+            pop.affinity_supress(progeny)
+            pop.add_rand_pop()
+            print('gen: {}, error:{}'.format(gen, self.best.get_error()))
             gen += 1
-        return best
