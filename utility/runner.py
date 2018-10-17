bb97c359428d185e7f51b60f04d78c0aeb2caf2c
ricmtz <ricmtz15@gmail.com>
2018-10-17 16:25:29 -0500

    #38 run_algorithm_p method implemented

--- utility/runner.py
+++ utility/runner.py
@@ -22,3 +22,21 @@ class Runner:
         Plotter.plot_result(*best.get_values(), algorithm.get_error(), name)
         Saver.save_results(name, best.get_values(),
                            algorithm.get_error(), total_time)
+
+    @staticmethod
+    def run_algorithm_p(algorithm, name=''):
+        print(name.center(WIDTH_TXT, FILL_CHR))
+        start = time.time()
+        algorithm.search()
+        finish = time.time()
+        bests = algorithm.get_bests()
+        total_time = time.strftime('%M:%S', time.gmtime(finish - start))
+        print(''.center(WIDTH_TXT, FILL_CHR))
+        print('Results'.center(WIDTH_TXT, FILL_CHR))
+        for i, best in bests.items():
+            print('id: {}, R:{}, L:{}, J:{}, Lambda:{}'.format(
+                i, *best.get_values()))
+        print('Time: {}'.format(total_time))
+        print(''.center(WIDTH_TXT, FILL_CHR))
+        best = min(bests.values(), key=lambda x: x.get_error())
+        Plotter.plot_result_p(*best.get_values(), algorithm.get_errors(), name)
