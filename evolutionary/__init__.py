b4bab2fea90c99a5a44e1cb1e127e1cfa85dae92
ricmtz <ricmtz15@gmail.com>
2018-10-17 16:31:03 -0500

    #38 GeneticParallel and GenParallelAlgorithm class added to all variable

--- evolutionary/__init__.py
+++ evolutionary/__init__.py
@@ -5,6 +5,9 @@ from .pop_dif import PopulationDif
 from .genetic import Genetic
 from .evolution_s import EvolutionStrat
 from .diff_evo import DiffEvolution
+from .genetic_p import GeneticParallel
+from .gen_p_algorithm import GenParallelAlgorithm
 
 __all__ = ['CitizenStrat', 'PopulationGen', 'PopulationStrat',
-           'PopulationDif', 'Genetic', 'EvolutionStrat', 'DiffEvolution']
+           'PopulationDif', 'Genetic', 'EvolutionStrat', 'DiffEvolution',
+           'GeneticParallel', 'GenParallelAlgorithm']
