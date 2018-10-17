401be4c0931468a21fabf02d100bc750169a0634
ricmtz <ricmtz15@gmail.com>
2018-10-17 16:23:53 -0500

    #38 AlgoritihmP class added to all variable

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 from .citizen import Citizen
 from .population import Population
 from .algorithm import Algorithm
+from .algorithm_p import AlgorithmP
 
-__all__ = ['Citizen', 'Population', 'Algorithm']
+__all__ = ['Citizen', 'Population', 'Algorithm', 'AlgorithmP']
