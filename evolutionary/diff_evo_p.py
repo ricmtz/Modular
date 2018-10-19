43d36aeb89d983dbbc8fa264c917454c5cd75ece
ricmtz <ricmtz15@gmail.com>
2018-10-19 17:26:20 -0500

    #39 DiffEvoParallel class modifed to inherit the Parallel class

--- evolutionary/diff_evo_p.py
+++ evolutionary/diff_evo_p.py
@@ -1,41 +1,16 @@
-from threading import Thread, RLock
-from models import Algorithm
+from models import Parallel
 from evolutionary import PopulationDif
 from random import sample
 
 
-class DiffEvoParallel(Thread, Algorithm):
-
-    DATA = {}
+class DiffEvoParallel(Parallel):
 
     def __init__(self, pop_size, max_gen, wf, cr, id_a):
-        super(DiffEvoParallel, self).__init__()
-        super(Thread, self).__init__()
-        super(Algorithm, self).__init__()
+        super().__init__(id_a)
         self.pop_size = pop_size
         self.max_gen = max_gen
         self.wf = wf
         self.cr = cr
-        self.id_a = id_a
-        self.lock = RLock()
-
-    def export_data(self, pop):
-        self.lock.acquire()
-        try:
-            self.DATA[self.id_a] = pop.copy()
-        finally:
-            self.lock.release()
-
-    def get_data(self):
-        pop = []
-        self.lock.acquire()
-        try:
-            for id_a, ctz in self.DATA.items():
-                if id_a != self.id_a:
-                    pop.extend(ctz)
-        finally:
-            self.lock.release()
-        return pop
 
     def run(self):
         gen = 0
@@ -45,7 +20,7 @@ class DiffEvoParallel(Thread, Algorithm):
         self.best = pop.get_best()
         while gen < self.max_gen:
             children = pop.create_children()
-            self.export_data(children) 
+            self.export_data(children)
             children.extend(self.get_data())
             children = sample(children, self.pop_size)
             pop.evaluate(children)
