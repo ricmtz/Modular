38db79ab1a227a6bba5253fbf4035e28051afd33
ricmtz <ricmtz15@gmail.com>
2018-10-21 20:28:02 -0500

    #42 UIEvolutionary, UIImmune, UIPhysical, UIProbabilistic, UIStochastic, UISwarm, UIParallel and App classes added to all variable

---
+++ ui/__init__.py
@@ -0,0 +1,12 @@
+from .ui_evo import UIEvolutionary
+from .ui_immune import UIImmune
+from .ui_physical import UIPhysical
+from .ui_probabilistic import UIProbabilistic
+from .ui_stochastic import UIStochastic
+from .ui_swarm import UISwarm
+from .ui_parallel import UIParallel
+from .app import App
+
+__all__ = ['UIEvolutionary', 'UIImmune', 'UIPhysical',
+           'UIProbabilistic', 'UIStochastic', 'UISwarm',
+           'UIParallel', 'App']
