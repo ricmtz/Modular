750b79d2dac8b0c7ff0ae31bca90b759b3dfd564
ricmtz <ricmtz15@gmail.com>
2018-10-21 20:20:45 -0500

    #42 main file created to run the app

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from ui import App
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = App()
+    ex.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
