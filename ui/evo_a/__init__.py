7fb1819418d264fc56b3f653ade032bc168546e5
ricmtz <ricmtz15@gmail.com>
2018-10-21 20:29:40 -0500

    #42 UIDiffEvolution, UIEvolutionStrat and UIGenetic classes added to all variable

---
+++ ui/evo_a/__init__.py
@@ -0,0 +1,5 @@
+from .ui_genetic import UIGenetic
+from .ui_diff_evo import UIDiffEvolution
+from .ui_evo_strat import UIEvolutionStrat
+
+__all__ = ['UIGenetic', 'UIDiffEvolution', 'UIEvolutionStrat']
