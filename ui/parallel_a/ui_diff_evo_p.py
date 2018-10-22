9a8e6bf1fc6b28c294979e98d539fc7cd62e13dd
ricmtz <ricmtz15@gmail.com>
2018-10-21 20:33:14 -0500

    #42 UIDiffEvolutionParallel and UIGeneticParallel classes implemented

---
+++ ui/parallel_a/ui_diff_evo_p.py
@@ -0,0 +1,38 @@
+from PyQt5.QtWidgets import QLabel, QLineEdit
+from ui.model import Form
+from evolutionary import DiffEvoAlgorithm
+from utility import Runner
+
+MAX_GEN = 5
+POP_SIZE = 30
+WEIGHT_F = 0.2
+CROSS_R = 0.7
+NUM_THREADS = 3
+
+
+class UIDiffEvolutionParallel(Form):
+    def __init__(self):
+        super().__init__('Differential Evolution Parallel')
+
+    def generate_fields(self):
+        self.max_gen = QLineEdit(str(MAX_GEN))
+        self.pop_size = QLineEdit(str(POP_SIZE))
+        self.weight_f = QLineEdit(str(WEIGHT_F))
+        self.cross_r = QLineEdit(str(CROSS_R))
+        self.num_threads = QLineEdit(str(NUM_THREADS))
+        self.form.addRow(QLabel('Max. generations:'), self.max_gen)
+        self.form.addRow(QLabel('Population size:'), self.pop_size)
+        self.form.addRow(QLabel('Weighting factor:'), self.weight_f)
+        self.form.addRow(QLabel('Crossover rate:'), self.cross_r)
+        self.form.addRow(QLabel('Num. threads:'), self.num_threads)
+
+    def run_algorithm(self):
+        max_gen = int(self.max_gen.text())
+        pop_size = int(self.pop_size.text())
+        weight_f = float(self.weight_f.text())
+        cross_r = float(self.cross_r.text())
+        num_threads = int(self.num_threads.text())
+        algorithm = DiffEvoAlgorithm(
+            pop_size, max_gen, weight_f, cross_r, num_threads)
+        bests, t_time = Runner.run_algorithm_p(algorithm, self.title)
+        self.print_results_p(bests, t_time)
---
+++ ui/parallel_a/ui_genetic_p.py
@@ -0,0 +1,38 @@
+from PyQt5.QtWidgets import QLabel, QLineEdit
+from ui.model import Form
+from evolutionary import GenParallelAlgorithm
+from utility import Runner
+
+POP_SIZE = 20
+BEST_P = 10
+MAX_GEN = 5
+P_M = 5
+NUM_THREADS = 3
+
+
+class UIGeneticParallel(Form):
+    def __init__(self):
+        super().__init__('Genetic evolution parallel')
+
+    def generate_fields(self):
+        self.max_gen = QLineEdit(str(MAX_GEN))
+        self.pop_size = QLineEdit(str(POP_SIZE))
+        self.best_pop = QLineEdit(str(BEST_P))
+        self.p_m = QLineEdit(str(P_M))
+        self.num_threads = QLineEdit(str(NUM_THREADS))
+        self.form.addRow(QLabel('Max. generations:'), self.max_gen)
+        self.form.addRow(QLabel('Population size:'), self.pop_size)
+        self.form.addRow(QLabel('Best population:'), self.best_pop)
+        self.form.addRow(QLabel('Percent mutant:'), self.p_m)
+        self.form.addRow(QLabel('Num. threads:'), self.num_threads)
+
+    def run_algorithm(self):
+        pop_size = int(self.pop_size.text())
+        best_p = int(self.best_pop.text())
+        max_gen = int(self.max_gen.text())
+        p_m = float(self.p_m.text())
+        num_threads = int(self.num_threads.text())
+        algorithm = GenParallelAlgorithm(
+            pop_size, best_p, max_gen, p_m, num_threads)
+        bests, t_time = Runner.run_algorithm_p(algorithm, self.title)
+        self.print_results_p(bests, t_time)
