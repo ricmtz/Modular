cffab41514b2a7f196bf1032227fddb2a84a8b0d
ricmtz <ricmtz15@gmail.com>
2018-10-21 20:36:13 -0500

    #42 UIAdaptativeRandomS, UIHillClimbing and UIRandomSearch  classes added to all variable

---
+++ ui/stochastic_a/__init__.py
@@ -0,0 +1,5 @@
+from .ui_adaptative_random_s import UIAdaptativeRandomS
+from .ui_hill_climbing import UIHillClimbing
+from .ui_random_s import UIRandomSearch
+
+__all__ = ['UIAdaptativeRandomS', 'UIHillClimbing', 'UIRandomSearch']
